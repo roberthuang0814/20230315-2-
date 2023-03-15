ae3a84b05035149aa2e4ade7559caa7e9d25f6e6
rober <rober@LAPTOP-9BJTS009>
2023-03-15 20:00:42 +0800

    加入專案檔案。

---
+++ 20230315題目2英哩轉公里/20230315題目2英哩轉公里.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>  
+#include <iomanip>  
+using namespace std;
+
+int main()
+{
+    int n;
+    while (cin >> n) {
+        double ans = n * 1.6;    
+        cout << fixed << setprecision(1) << ans << "\n";
+    }
+
+    return 0;
+}
